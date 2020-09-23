6ff0c76863a67f317878581b54ec683ca6d641f5
Pratik <pratik1070061@gmail.com>
2020-09-22 23:07:50 -0400

    adding meta styles

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 const nodeList = [];
+const dpi = 220;
+
+const pageStyles = {
+  margins: {
+    left: 220 * (3 / 4),
+    right: 220 * (3 / 4),
+    top: 220 * (3 / 4),
+    bottom: 220 * (3 / 4),
+  },
+  highlights: {
+    "rgb(255, 0, 0)": {
+      justify: "left",
+      fontFamily: "Albany AMT",
+      fontSize: "8pt",
+      letterCasing: "Title",
+    },
+    "rgb(255, 255, 0)": {
+      justify: "center",
+      fontFamily: "Albany AMT",
+      fontSize: "12pt",
+      fontWeight: "Bold",
+      letterCasing: "Capital",
+    },
+    lightGray: {
+      justify: "justify",
+      fontFamily: "Albany AMT",
+      fontSize: "10pt",
+      fontWeight: "Bold",
+      letterCasing: "Capital",
+    },
+    "rgb(0, 0, 255)": {
+      fontWeight: "bold",
+    },
+    "rgb(0, 255, 0)": {
+      justify: "justify",
+      fontFamily: "Albany AMT",
+      fontSize: "10pt",
+      fontWeight: "Bold",
+      letterCasing: "Title",
+    },
+    darkYellow: {
+      justify: "justify",
+      fontFamily: "Albany AMT",
+      fontSize: "10pt",
+      fontWeight: "Bold",
+      letterCasing: "Capital",
+    },
+    pink: {
+      marginRight: "10pt",
+    },
+    turquoise: {
+      marginRight: "12pt",
+    },
+  },
+};
 
 const getContentNodes = (node, index, parentIndex) => {
   if (node.nodeType !== Node.TEXT_NODE) {
@@ -13,7 +68,7 @@ const getContentNodes = (node, index, parentIndex) => {
         fontFamily: styles.fontFamily,
         fontWeight: styles.fontWeight,
         fontSize: styles.fontSize,
-        backgroundColor: styles.backgroundColor
+        backgroundColor: styles.backgroundColor,
       },
       parentIndex: parentIndex,
     });
@@ -23,6 +78,7 @@ const getContentNodes = (node, index, parentIndex) => {
       }
     }
   } else {
+    const style = nodeList[nodeList.length - 1].styles;
     const ele = {
       node: node,
       text: node.nodeValue,
@@ -30,8 +86,8 @@ const getContentNodes = (node, index, parentIndex) => {
       parentIndex: parentIndex,
       location: nodeList[nodeList.length - 1]["0"],
       styles: nodeList[nodeList.length - 1].styles,
+      metaSyles: pageStyles.highlights[style.backgroundColor],
     };
-    console.log(ele, nodeList[nodeList.length - 1]["0"]);
     nodeList.push(ele);
   }
 };
@@ -40,7 +96,18 @@ const getContentNodes = (node, index, parentIndex) => {
 const startParsing = () => {
   try {
     getContentNodes(document.querySelector(".WordSection1"), 0, 0);
-    console.log(nodeList.filter((x) => x.location));
+    console.log(
+      nodeList
+        .filter((x) => x.location)
+        .map((x) => {
+          return {
+            text: x.text,
+            location: x.location,
+            styles: x.styles,
+            metaSyles: x.metaSyles,
+          };
+        })
+    );
   } catch (err) {
     console.log(err);
   }
